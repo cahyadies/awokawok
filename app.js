5a47135bd62aefbf678ce2ee3fdefc3a10d17371
Diananaa <d1anasite04@gmail.com>
2022-01-31 16:57:16 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const app = () => {
+  return(
+    <View>
+      <Text>Hello app</Text>
+    </View>
+  )
+}
+export default app;
+
+const styles = StyleSheet.create({})
